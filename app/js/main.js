34f6322c93e535c3fba192a993ff7bb7c65003f1
Garnata <garnata28@gmail.com>
2021-03-18 22:05:42 +0200

    added products + design

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,16 @@ $(function(){
         autoplaySpeed: 2000
     });
 
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
+
 });
