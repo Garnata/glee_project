13589399766be16598adbeaebf6a044e9239663d
Garnata <garnata28@gmail.com>
2021-03-31 23:28:27 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,42 @@
 $(function(){
 
+    $('.related__items').slick({
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.product-tabs__top-item').on('click', function (e) {
+        e.preventDefault();
+        $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+        $(this).addClass('product-tabs__top-item--active');
+
+        $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+        $($(this).attr('href')).addClass('product-tabs__content-item--active');
+    })
+
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+    });
+
+    $('.product__item-num').styler();
+
     $('.filter-price__input').ionRangeSlider({
         type: "double",
         prefix: "$"
-
-
     });
 
     $('.top-slider__inner').slick({
